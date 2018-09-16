bff20e3c9b132258960b98b049d22100a54ec9e7
Énekes Ábel <enekes.abel@gmail.com>
2018-09-16 23:43:42 +0200

    vue-material added

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,77 @@
 <template>
-    <div id="app">
-        <div id="nav">
-            <router-link to="/">Home</router-link>
-            |
-            <router-link to="/about">About</router-link>
-        </div>
-        <router-view/>
-    </div>
+    <md-app md-waterfall md-mode="fixed-last">
+        <md-app-toolbar class="md-large md-dense md-primary">
+            <div class="md-toolbar-row">
+                <div class="md-toolbar-section-start">
+                    <md-button class="md-icon-button" @click="menuVisible = !menuVisible">
+                        <md-icon>menu</md-icon>
+                    </md-button>
+
+                    <span class="md-title">My Title</span>
+                </div>
+
+                <div class="md-toolbar-section-end">
+                    <md-button class="md-icon-button">
+                        <md-icon>more_vert</md-icon>
+                    </md-button>
+                </div>
+            </div>
+
+            <div class="md-toolbar-row">
+                <md-tabs class="md-primary">
+                    <md-tab id="tab-home" md-label="Home"></md-tab>
+                    <md-tab id="tab-pages" md-label="Pages"></md-tab>
+                    <md-tab id="tab-posts" md-label="Posts"></md-tab>
+                    <md-tab id="tab-favorites" md-label="Favorites"></md-tab>
+                </md-tabs>
+            </div>
+        </md-app-toolbar>
+
+        <md-app-drawer :md-active.sync="menuVisible">
+            <md-toolbar class="md-transparent" md-elevation="0">Navigation</md-toolbar>
+
+            <md-list>
+                <md-list-item>
+                    <md-icon>move_to_inbox</md-icon>
+                    <span class="md-list-item-text">Inbox</span>
+                </md-list-item>
+
+                <md-list-item>
+                    <md-icon>send</md-icon>
+                    <span class="md-list-item-text">Sent Mail</span>
+                </md-list-item>
+
+                <md-list-item>
+                    <md-icon>delete</md-icon>
+                    <span class="md-list-item-text">Trash</span>
+                </md-list-item>
+
+                <md-list-item>
+                    <md-icon>error</md-icon>
+                    <span class="md-list-item-text">Spam</span>
+                </md-list-item>
+            </md-list>
+        </md-app-drawer>
+
+        <md-app-content>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error quibusdam, non molestias et! Earum magnam, similique, quo recusandae placeat dicta asperiores modi sint ea repudiandae maxime? Quae non explicabo, neque.</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error quibusdam, non molestias et! Earum magnam, similique, quo recusandae placeat dicta asperiores modi sint ea repudiandae maxime? Quae non explicabo, neque.</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error quibusdam, non molestias et! Earum magnam, similique, quo recusandae placeat dicta asperiores modi sint ea repudiandae maxime? Quae non explicabo, neque.</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error quibusdam, non molestias et! Earum magnam, similique, quo recusandae placeat dicta asperiores modi sint ea repudiandae maxime? Quae non explicabo, neque.</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error quibusdam, non molestias et! Earum magnam, similique, quo recusandae placeat dicta asperiores modi sint ea repudiandae maxime? Quae non explicabo, neque.</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error quibusdam, non molestias et! Earum magnam, similique, quo recusandae placeat dicta asperiores modi sint ea repudiandae maxime? Quae non explicabo, neque.</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error quibusdam, non molestias et! Earum magnam, similique, quo recusandae placeat dicta asperiores modi sint ea repudiandae maxime? Quae non explicabo, neque.</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error quibusdam, non molestias et! Earum magnam, similique, quo recusandae placeat dicta asperiores modi sint ea repudiandae maxime? Quae non explicabo, neque.</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error quibusdam, non molestias et! Earum magnam, similique, quo recusandae placeat dicta asperiores modi sint ea repudiandae maxime? Quae non explicabo, neque.</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error quibusdam, non molestias et! Earum magnam, similique, quo recusandae placeat dicta asperiores modi sint ea repudiandae maxime? Quae non explicabo, neque.</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error quibusdam, non molestias et! Earum magnam, similique, quo recusandae placeat dicta asperiores modi sint ea repudiandae maxime? Quae non explicabo, neque.</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error quibusdam, non molestias et! Earum magnam, similique, quo recusandae placeat dicta asperiores modi sint ea repudiandae maxime? Quae non explicabo, neque.</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error quibusdam, non molestias et! Earum magnam, similique, quo recusandae placeat dicta asperiores modi sint ea repudiandae maxime? Quae non explicabo, neque.</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error quibusdam, non molestias et! Earum magnam, similique, quo recusandae placeat dicta asperiores modi sint ea repudiandae maxime? Quae non explicabo, neque.</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error quibusdam, non molestias et! Earum magnam, similique, quo recusandae placeat dicta asperiores modi sint ea repudiandae maxime? Quae non explicabo, neque.</p>
+        </md-app-content>
+    </md-app>
 </template>
 <script lang="ts" src="./App.ts"/>
 <style lang="scss">
-    #app {
-        font-family: 'Avenir', Helvetica, Arial, sans-serif;
-        -webkit-font-smoothing: antialiased;
-        -moz-osx-font-smoothing: grayscale;
-        text-align: center;
-        color: #2c3e50;
-    }
-
-    #nav {
-        padding: 30px;
-        a {
-            font-weight: bold;
-            color: #2c3e50;
-            &.router-link-exact-active {
-                color: #42b983;
-            }
-        }
-    }
 </style>
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,20 @@
+// styles
+import 'vue-material/dist/vue-material.min.css';
+import 'vue-material/dist/theme/default.css';
+
+// scripts
 import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
 
+// using full material bundle till ts type definitions arrive
+import VueMaterial from 'vue-material';
+
 Vue.config.productionTip = false;
+
+// plugins
+Vue.use(VueMaterial);
 
 new Vue({
 	router,
