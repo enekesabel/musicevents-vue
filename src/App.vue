e17035a94b423d70ccb356d911396abe2eaa3130
Énekes Ábel <enekes.abel@gmail.com>
2018-10-31 20:01:53 +0100

    drawer and tabs renamed

--- src/App.vue
+++ src/App.vue
@@ -39,37 +39,23 @@
             <div class="d-none d-sm-block">
                 <div class="md-toolbar-row">
                     <md-tabs class="md-primary">
-                        <md-tab id="tab-home" md-label="Home"></md-tab>
-                        <md-tab id="tab-pages" md-label="Pages"></md-tab>
-                        <md-tab id="tab-posts" md-label="Posts"></md-tab>
-                        <md-tab id="tab-favorites" md-label="Favorites"></md-tab>
+                        <md-tab id="tab-home" md-label="My Artists"></md-tab>
+                        <md-tab id="tab-pages" md-label="My Events"></md-tab>
                     </md-tabs>
                 </div>
             </div>
         </md-app-toolbar>
 
         <md-app-drawer :md-active.sync="menuVisible">
-            <md-toolbar class="md-transparent" md-elevation="0">Navigation</md-toolbar>
-
             <md-list>
                 <md-list-item>
-                    <md-icon>move_to_inbox</md-icon>
-                    <span class="md-list-item-text">Inbox</span>
+                    <md-icon>recent_actors</md-icon>
+                    <span class="md-list-item-text">My Artists</span>
                 </md-list-item>
 
                 <md-list-item>
-                    <md-icon>send</md-icon>
-                    <span class="md-list-item-text">Sent Mail</span>
-                </md-list-item>
-
-                <md-list-item>
-                    <md-icon>delete</md-icon>
-                    <span class="md-list-item-text">Trash</span>
-                </md-list-item>
-
-                <md-list-item>
-                    <md-icon>error</md-icon>
-                    <span class="md-list-item-text">Spam</span>
+                    <md-icon>event_note</md-icon>
+                    <span class="md-list-item-text">My Events</span>
                 </md-list-item>
             </md-list>
         </md-app-drawer>
