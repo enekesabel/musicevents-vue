d620e3c70ea7dac0fb53c1936be6f8570e9aa89f
Énekes Ábel <enekes.abel@gmail.com>
2018-11-01 13:07:37 +0100

    About component and route removed

--- src/router/index.ts
+++ src/router/index.ts
@@ -29,13 +29,5 @@ export default new Router({
       name: Routes.EVENT_LIST,
       component: () => import('../views/event_list/EventList.vue'),
     },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/about/About.vue'),
-    },
   ],
 });
