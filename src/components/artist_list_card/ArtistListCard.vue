304b62e5d12c872bc4b72d84d7b63c887c8d1d82
Énekes Ábel <enekes.abel@gmail.com>
2018-11-01 15:34:26 +0100

    ArtistListCard item created

---
+++ src/components/artist_list_card/ArtistListCard.vue
@@ -0,0 +1,25 @@
+<template>
+    <md-card>
+        <md-card-media-cover md-solid>
+            <md-card-media md-ratio="16:9">
+                <img :src="artist.imageUrl" :alt="artist.name"/>
+            </md-card-media>
+
+            <md-card-area>
+                <md-card-header>
+                    <span class="md-title">{{artist.name}}</span>
+                </md-card-header>
+
+                <md-card-actions>
+                    <md-button class="md-icon-button"
+                               @click="onFavouriteClick">
+                        <md-icon>favorite</md-icon>
+                    </md-button>
+                </md-card-actions>
+            </md-card-area>
+        </md-card-media-cover>
+    </md-card>
+</template>
+
+<script lang="ts" src="./ArtistListCard.ts">
+</script>
