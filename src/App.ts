24293baf04ea004bc15030ef7e3c00c2dee99e20
Énekes Ábel <enekes.abel@gmail.com>
2018-11-03 16:46:22 +0100

    MainToolbarRow component extracted, manages ArtistSearch & drawer visibility on mobile

--- src/App.ts
+++ src/App.ts
@@ -2,30 +2,15 @@ import Component, {mixins} from 'vue-class-component';
 import {Routes} from '@/router/routes';
 import {GeneralActionsMixin} from '@/mixins/GeneralActionsMixin';
 import {GeneralGettersMixin} from '@/mixins/GeneralGettersMixin';
+import MainToolbarRow from '@/components/main_toolbar_row/MainToolbarRow';
 
-@Component
+@Component({
+  components: {MainToolbarRow},
+})
 export default class App extends mixins(GeneralActionsMixin, GeneralGettersMixin) {
   private drawerVisible: boolean = false;
 
   Routes = Routes;
-
-  selectedArtist = null;
-  artists = [
-    'Jim Halpert',
-    'Dwight Schrute',
-    'Michael Scott',
-    'Pam Beesly',
-    'Angela Martin',
-    'Kelly Kapoor',
-    'Ryan Howard',
-    'Kevin Malone',
-    'Creed Bratton',
-    'Oscar Nunez',
-    'Toby Flenderson',
-    'Stanley Hudson',
-    'Meredith Palmer',
-    'Phyllis Lapin-Vance',
-  ];
 
   created() {
     this.calculateDeviceSize(window.innerWidth);
---
+++ src/components/main_toolbar_row/MainToolbarRow.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="md-toolbar-row">
+        <div class="md-toolbar-section-start">
+            <md-button v-if="isMobile"
+                       class="md-icon-button"
+                       @click="toggleDrawer">
+                <md-icon>menu</md-icon>
+            </md-button>
+
+            <span v-if="!isMobile || isMobile && !searchShown" class="md-title">My Title</span>
+        </div>
+
+        <div class="md-toolbar-section-end">
+            <div>
+                <div v-if="searchShown"
+                     class="d-flex flex-row">
+                    <artist-search></artist-search>
+                    <md-icon v-if="isMobile" @click.native="hideSearch">arrow_back</md-icon>
+                </div>
+                <md-icon v-else
+                         @click.native="showSearch">
+                    search
+                </md-icon>
+            </div>
+        </div>
+    </div>
+</template>
+<script lang="ts" src="./MainToolbarRow.ts"/>
+<style lang="scss">
+
+</style>
