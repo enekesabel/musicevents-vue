879987f8d866a16e8cae73f2e78b8f9b3a393154
Ábel Énekes <enekes.abel@gmail.com>
2019-05-12 23:56:53 +0200

    EventListItem i updated, lineup displayed

--- src/components/event_list_item/EventListItem.vue
+++ src/components/event_list_item/EventListItem.vue
@@ -1,18 +1,23 @@
 <template>
-    <md-list-item md-expand>
+    <md-list-item :md-expand="hasOtherArtists">
         <event-favourite-button class="ml-0 mr-3" :event="event"></event-favourite-button>
         <div class="md-list-item-text">
             <span>{{event.locationName}}</span>
-            <span>Artist: {{event.artistId}}</span>
-            <p>{{event.datetime}}</p>
+            <span>{{event.city}}</span>
+            <p>{{event.datetime | parseDate}}</p>
         </div>
 
-        <md-list slot="md-expand">
+        <md-list v-if="hasOtherArtists" slot="md-expand">
             <md-list-item
                     class="md-inset">
-                <div class="md-list-item-text">
-                    <div>{{event.city}}</div>
-                    <div v-if="description">{{description}}</div>
+                <div class="d-flex flex-column">
+                    <div class="md-subheading">Lineup</div>
+                    <div class="d-flex flex-column align-items-start">
+                        <a href="#" v-for="artist in event.lineup"
+                                   @click.prevent="onArtistClick(artist)"
+                                   class="md-primary md-body-1 my-1">{{artist}}
+                        </a>
+                    </div>
                 </div>
             </md-list-item>
         </md-list>
